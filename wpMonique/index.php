8b2ed790e89826ffccfe64cc709c4c36e0af1de5
Veronica K. B. Olsen <code@vkbo.net>
2016-11-21 23:09:11 +0100

    Initial files

---
+++ wpMonique/index.php
@@ -0,0 +1,14 @@
+<?php
+   /**
+    * Main template file
+    *
+    * @package WordPress
+    * @subpackage wpMonique
+    * @since wpMonique 0.1
+    */
+
+    global $cLang;
+
+    get_header();
+?>
+
