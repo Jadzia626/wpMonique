fcae6e3c718cddea2c063c7c68427a225a958342
Veronica K. B. Olsen <code@vkbo.net>
2016-12-13 23:27:53 +0100

    Added footer

--- wpMonique/footer.php
+++ wpMonique/footer.php
@@ -7,10 +7,27 @@
     * @since wpMonique 0.1
     */
 
+    global $sBlobTitle;
+    global $sBlobText;
 ?>
 
     </div>
     <!-- End Content -->
+
+    <!-- Begin Footer -->
+    <footer id="footer">
+        <div id="site-footer">
+            <div class="footer-left">
+                <h2><?php echo $sBlobTitle; ?></h2>
+                <?php echo $sBlobText; ?>
+            </div>
+            <div class="footer-right">
+                <h2>&nbsp;</h2>
+                <?php wp_nav_menu(array("theme_location"=>"footer","menu_class"=>"footer-menu")); ?>
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer -->
 
     </div>
     <!-- End Wrapper -->
